7d67b518aaffd9062e9c01712d3d11d4da77a54b
Farouq Umar <ufakz99@gmail.com>
2020-08-03 21:41:37 +0100

    feat: added session storage to state

--- src/store/index.js
+++ src/store/index.js
@@ -2,39 +2,45 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as fb from '../firebase'
 import router from '../router/index'
+import { createPersistedState } from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  plugins:[
+    createPersistedState({
+      storage: window.sessionStorage
+    })
+  ],
   state: {
-    userProfile:{},
-    userType:'',
+    userProfile: {},
+    userType: '',
     isLoggedIn: false
   },
   mutations: {
-    SET_USER_PROFILE(state,payload){
+    SET_USER_PROFILE(state, payload) {
       state.userProfile = payload
       state.isLoggedIn = true
     },
-    SET_USER_TYPE(state,payload){
+    SET_USER_TYPE(state, payload) {
       state.userType = payload
     }
   },
   actions: {
-    async login({commit},phoneNumber){
+    async login({ commit }, phoneNumber) {
       const customer = await fb.customersCollection.doc(phoneNumber).get()
       const trucker = await fb.truckersCollection.doc(phoneNumber).get()
 
-      if(customer.exists){
-        commit('SET_USER_PROFILE',customer.data())
-        commit('SET_USER_TYPE','customer')
+      if (customer.exists) {
+        commit('SET_USER_PROFILE', customer.data())
+        commit('SET_USER_TYPE', 'customer')
         router.push('/customer')
         return
       }
 
-      if(trucker.exists){
-        commit('SET_USER_PROFILE',trucker.data())
-        commit('SET_USER_TYPE','trucker')
+      if (trucker.exists) {
+        commit('SET_USER_PROFILE', trucker.data())
+        commit('SET_USER_TYPE', 'trucker')
         router.push('/trucker')
         return
       }
